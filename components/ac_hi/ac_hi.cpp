6ebdbfcb5a30e7f3f2b42df314a1de55ebbbd01a
Mike Russell <mikerussellnz@gmail.com>
2026-09-21 12:06:42 +0800

    debug

--- components/ac_hi/ac_hi.cpp
+++ components/ac_hi/ac_hi.cpp
@@ -1564,6 +1564,7 @@ bool ACHIClimate::extract_next_frame_(std::vector<uint8_t> &frame) {
 }
 
 void ACHIClimate::handle_frame_(const std::vector<uint8_t> &b) {
+  ESP_LOGD(TAG, "Handling frame (%u bytes)", (unsigned) b.size());
   ESP_LOG_BUFFER_HEX_LEVEL(TAG, b.data(), b.size(), ESP_LOG_INFO);
   if (b.size() < 20) {
     ESP_LOGD(TAG, "Frame too short (%u), ignored", (unsigned) b.size());
